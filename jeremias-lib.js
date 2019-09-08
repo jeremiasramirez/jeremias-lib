94c84f20aae9d31f723faa4f1425b586ecb9403b
Jeremias ramirez <jereramirez499@gmail.com>
2019-09-08 17:01:09 -0400

    Agregando X al elemento de cierre de elementos

--- jeremias-lib.js
+++ jeremias-lib.js
@@ -2,6 +2,7 @@
 		let close = document.querySelectorAll('.close');
 		if(close){
 			for(let closes=0; closes<close.length; closes+=1){
+				close[closes].textContent = "X";
 				close[closes].addEventListener("click", (e)=>{
 					close[closes].parentNode.remove();
 				})
